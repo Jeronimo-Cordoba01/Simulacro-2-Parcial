961addc545899049c46cf00bfae91c83b61048eb
Jeronimo-Cordoba01 <jerof.l.cordoba01@gmail.com>
2024-11-12 20:18:35 -0300

    FINISH HIM

--- Main.py
+++ Main.py
@@ -18,34 +18,13 @@ def opciones_menu() -> str:
         "3. Salir\n"
     )
 
-def mostrar_menu() -> None:
-    """
-    Muestra el menú principal y gestiona la opción seleccionada.
-    """
-    limpiar_pantalla()
-    print(opciones_menu())
-    opcion = input("Selecciona una opción: ")
-    
-    match opcion:
-        case "1":
-            jugar_juego()
-        case "2":
-            mostrar_rankings()
-        case "3":
-            terminar_juego("¡Gracias por jugar!")
-            return  
-        case _:
-            print("Opción no válida. Inténtalo de nuevo.")
-    input("Presiona Enter para continuar...")
-    limpiar_pantalla()
-
-def jugar_juego() -> None:
+def jugar_juego(lista_rankings:list) -> None:
     """
     Ejecuta la lógica principal del juego, administra preguntas y vidas, 
     y actualiza puntajes.
     """
     diccionario_juego = {"continuar": True}
-    lista_rankings = []
+    
     
 
     nombre = input("Ingresa tu nombre: ")
@@ -58,7 +37,7 @@ def jugar_juego() -> None:
     while verificar_estado_juego(diccionario_juego):
         for pregunta in preguntas_star_wars:
             # Verificar si ya se han respondido 10 preguntas
-            if pregunta_contador == 10:
+            if pregunta_contador == len(preguntas_star_wars):
                 print("¡Felicidades! Has completado las 10 preguntas.")
                 diccionario_juego["continuar"] = False
                 break
@@ -74,13 +53,15 @@ def jugar_juego() -> None:
             match respuesta_usuario:
                 case "1" | "2" | "3" | "4":
                     respuesta_usuario = pregunta["opciones"][int(respuesta_usuario) - 1]
-                    
+                    jugador_dict = jugador.__dict__
                     if respuesta_usuario == pregunta["respuesta"]:
                         print("¡Correcto!")
-                        modificar_puntaje(jugador.__dict__, jugador.puntaje + 1)
+                        
+                        modificar_puntaje(jugador_dict, jugador.puntaje + 1)
+                        print(jugador_dict)
                     else:
                         print(f"Incorrecto. La respuesta correcta era: {pregunta['respuesta']}")
-                        modificar_vidas(jugador.__dict__, jugador.vidas - 1)
+                        modificar_vidas(jugador_dict, jugador.vidas - 1)
 
                     match jugador.vidas:
                         case vidas if vidas <= 0:
@@ -89,18 +70,34 @@ def jugar_juego() -> None:
                             break
                 case _:
                     print("Opción inválida. Debes seleccionar un número entre 1 y 4.")
-                
-            input('Apretar para seguir con la siguiente pregunta....')
-            limpiar_pantalla()
+            print(diccionario_juego["continuar"])       
+            if diccionario_juego["continuar"]:   
+                input('Apretar para seguir con la siguiente pregunta....')
+                limpiar_pantalla()
 
-    print("El juego ha terminado.")
-    system("pause")
-    mostrar_menu()
-    # Guardar puntaje y mostrar ranking
-    guardar_puntuacion(lista_rankings, jugador.__dict__)
+    guardar_puntuacion(lista_rankings, jugador_dict)
     ordenar_puntaciones(lista_rankings, 'desc')
-    mostrar_rankings(lista_rankings)
     terminar_juego("TERMINÓ EL JUEGO")
 
-mostrar_menu()
-jugar_juego()
+
+
+lista_rankings = []   
+entrar = True
+while entrar:
+    
+    print(opciones_menu())
+    
+    opcion = input("Selecciona una opción: ")
+    match opcion:
+        case "1":
+            jugar_juego(lista_rankings)
+        case "2":
+            mostrar_rankings(lista_rankings)
+        case "3":
+            terminar_juego("¡Gracias por jugar!")
+            entrar = False  
+        case _:
+            print("Opción no válida. Inténtalo de nuevo.")
+    if entrar:
+        input("Presiona Enter para continuar...")
+        limpiar_pantalla()
--- Puntaje.py
+++ Puntaje.py
@@ -20,15 +20,20 @@ def ordenar_puntaciones(lista_rankings:list, criterio:str)->bool:
     if criterio == 'asc':
         for i in range(len(lista_rankings)):
             for j in range(len(lista_rankings)):
-                if lista_rankings[i] < lista_rankings[j]:
+                if lista_rankings[i]['puntaje'] < lista_rankings[j]['puntaje']:
                     lista_rankings[i], lista_rankings[j] = lista_rankings[j], lista_rankings[i]
     elif criterio == 'desc':
         for i in range(len(lista_rankings)):
             for j in range(len(lista_rankings)):
-                if lista_rankings[i] > lista_rankings[j]:
+                if lista_rankings[i]['puntaje'] > lista_rankings[j]['puntaje']:
                     lista_rankings[i], lista_rankings[j] = lista_rankings[j], lista_rankings[i]
 
 def mostrar_rankings(lista_rankings:list)->bool:
     print('**********RANKINGS************')
-    for puntaje in lista_rankings:
-        print(f'El puntaje : {puntaje}')
+    if len(lista_rankings) > 0:
+        print(f'| {"Puesto":<6} | {"Puntaje":<7} | {"Nombre":<10} | {"Identificador":<14} |')
+        print(f'| {"-"*6} | {"-"*7} | {"-"*10} | {"-"*14} |')
+        for i, puntaje in enumerate(lista_rankings, 1):
+            print(f"| {i:<6} | {puntaje['puntaje']:<7} | {puntaje['nombre']:<10} | {puntaje['identificador']:<14} |")
+    else:
+        print('La lista está vacía')
