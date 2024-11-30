b046f830ac087e651275285bca37ed4e52e25fcd
Jeronimo-Cordoba01 <jerof.l.cordoba01@gmail.com>
2024-11-30 05:43:50 -0300

    cositas

--- utilidades.py
+++ utilidades.py
@@ -1,8 +1,7 @@
 import os
 
 def limpiar_pantalla():
-    # Limpia la pantalla basado en el sistema operativo
-    if os.name == 'nt':  # Para Windows
+    if os.name == 'nt':
         os.system('cls')
-    else:  # Para Mac y Linux (posix)
+    else:
         os.system('clear')
